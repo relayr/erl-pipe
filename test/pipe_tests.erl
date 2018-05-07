d645565bb83f46acfc598a65317e81cf603ea84c
Kuba Odias <kuba.odias@relayr.io>
2018-05-07 22:59:17 +0200

    Remove dependency from pdm-testutils

--- test/pipe_tests.erl
+++ test/pipe_tests.erl
@@ -9,23 +9,18 @@
 -module(pipe_tests).
 -author("sjanota").
 
-%% API
--export([]).
-
--include_lib("testutils/include/testing.hrl").
-
 -ifdef(TEST).
 
-?TEST_FUN().
-pipe_lists_operations() ->
+-include_lib("eunit/include/eunit.hrl").
+
+pipe_lists_operations_test() ->
     Result = pipe:tail([1,2,3,4], [
         {fun lists:filter/2, [fun(I) -> I rem 2 == 0 end]},
         {fun lists:foldl/3, [fun(I, Acc) -> Acc + I end, 0]}
     ]),
     ?assertEqual(6, Result).
 
-?TEST_FUN().
-flat_ok() ->
+flat_ok_test() ->
     Result = pipe:head(2, [
         fun(N) -> {ok, N} end
     ], [flat_ok]),
@@ -36,8 +31,7 @@ flat_ok() ->
     ], [flat_ok]) end,
     ?assertError({bad_return, 2}, BadReturn()).
 
-?TEST_FUN().
-until() ->
+until_test() ->
     Increment = fun(N) -> N+1 end,
     GraterThen = fun(Max) -> fun(N) ->
         if N > Max -> {false, {error, N}}; true -> true end
@@ -47,8 +41,7 @@ until() ->
     ], [{until, GraterThen(2)}]),
     ?assertEqual({error, 3}, Result).
 
-?TEST_FUN().
-until_with_flat_ok() ->
+until_with_flat_ok_test() ->
     Increment = fun(N) -> {ok, N+1} end,
     GraterThen = fun(Max) -> fun({ok, N}) ->
         if N > Max -> {false, {error, N}}; true -> true end
@@ -58,14 +51,12 @@ until_with_flat_ok() ->
     ], [flat_ok, {until, GraterThen(2)}]),
     ?assertEqual({error, 3}, Result).
 
-?TEST_FUN().
-multi_state() ->
+multi_state_test() ->
     Increment = fun(N1, N2) -> {N1+1, N2+1} end,
     Result = pipe:head({2,3}, [Increment, Increment], [multi_state]),
     ?assertEqual({4,5}, Result).
 
-?TEST_FUN().
-multi_state_with_flat_ok() ->
+multi_state_with_flat_ok_test() ->
     Increment = fun(N1, N2) -> {ok, N1+1, N2+1} end,
     Result = pipe:head({2,3}, [Increment, Increment], [multi_state, flat_ok]),
     ?assertEqual({4,5}, Result).
